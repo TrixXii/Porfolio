c90254b87c479eab0908b08a9289f08d0d10ccc9
Beatriz Muñoz <bellatrixX738@gmail.com>
2023-04-26 13:30:59 +0200

    rrr

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    sendEmail();
+});
+
+function sendEmail() {
+    const recipient = 'beatrizmunozabellan@gmail.com';
+    const subject = `Nuevo mensaje de ${nameInput.value}`;
+    const body = `Nombre: ${nameInput.value}\nCorreo electrónico: ${emailInput.value}\nMensaje: ${messageInput.value}`;
+    const mailtoUrl = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoUrl;
+}
+
---
+++ js/script.js
@@ -0,0 +1,56 @@
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('fade-in');
+        } else {
+            entry.target.classList.remove('fade-in');
+        }
+    });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el)
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
