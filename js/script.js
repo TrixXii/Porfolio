2688c56f1e4be8de7e0e825dba845f638a49f31e
TrixXii <bellatrixX738@gmail.com>
2023-04-27 19:22:55 +0200

    dddd

--- js/script.js
+++ js/script.js
@@ -65,3 +65,29 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('scroll');
     }
 });
+
+const button = document.querySelector(".main-circle");
+const circles = document.querySelectorAll(".circles");
+const cross = document.querySelector(".cross");
+const crossbg = document.querySelector(".bg");
+
+button.addEventListener("click", function() {
+    cross.classList.toggle("show");
+    crossbg.classList.toggle("show");
+    circles.forEach(element => {
+        element.classList.toggle("show");
+    });
+
+});
+
+const button2 = document.querySelector(".main-circle2");
+const cross2 = document.querySelector(".cross2");
+
+button2.addEventListener("click", function() {
+    cross2.classList.toggle("show2");
+    crossbg.classList.toggle("show2");
+    circles.forEach(element => {
+        element.classList.toggle("show2");
+    });
+
+});
